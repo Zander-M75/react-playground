c47caa0ddb135eaeeca78b1167ef04981620d0a5
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-09-30 17:18:34 -0400

    react app created

---
+++ react-playground/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>This is my first React + Vite project</h1>
+        <h2>More coming soon</h2>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
